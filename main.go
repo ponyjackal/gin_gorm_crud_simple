4d9719d9e874d902fa512292e81ee7371ce6fe54
ponyjackal <pony1125@outlook.com>
2023-04-10 16:55:01 -0700

    feat: read data from db

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 
+	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
+var db *gorm.DB
+var err error
 type Person struct {
 	Id uint `json:"id"`
 	FirstName string `jsong:"firstname"`
@@ -14,20 +17,31 @@ type Person struct {
 }
 
 func main() {
-	db, _ := gorm.Open("sqlite3", "./gorm.db")
+	db, err = gorm.Open("sqlite3", "./gorm.db")
+	if err != nil {
+		fmt.Println(err)
+	}
 	defer db.Close()
 
 	db.AutoMigrate(&Person{})
 
-	p1 := Person{FirstName: "John", LastName: "Doe"}
-	p2 := Person{FirstName: "Jane", LastName: "Smith"}
+	var people []Person
 
-	db.Create(&p1)
+	db.Find(&people)
 
-	var p3 Person
-	db.First(&p3)
+	r := gin.Default()
+	r.GET("/", GetProjects)
 
-	fmt.Println(p1.FirstName)
-	fmt.Println(p2.LastName)
-	fmt.Println(p3.LastName)
+	r.Run(":8080")
+}
+
+func GetProjects(c *gin.Context) {
+	var people []Person
+
+	if err := db.Find(&people).Error; err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	} else {
+		c.JSON(200, people)
+	}
 }
